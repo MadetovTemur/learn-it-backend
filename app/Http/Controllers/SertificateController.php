ba8d70135b1703d681fd8841a49b660eececa443
ziffler 7 <you@example.com>
2024-06-24 16:03:19 +0300

    updaeting system

--- app/Http/Controllers/SertificateController.php
+++ app/Http/Controllers/SertificateController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\{SertificateDiscriptions, Sertificate};
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Redirect;
-
+use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Str;
 
 
@@ -15,49 +15,16 @@ class SertificateController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index1(Request $request)
+    public function index(Request $request)
     {
-        $query = $request->query();
 
-        if (array_key_exists('sort', $query) and empty($query['sort'])) {
-            $sertificates = Sertificate::paginate(50);
-        } else if(array_key_exists('sort', $query) and $query['sort'][0] == '-') {
-            $sertificates = Sertificate::orderByDesc(str_replace('-', '', $query['sort']))->paginate(50);
-        }elseif(array_key_exists('sort', $query)) {
-            $sertificates = Sertificate::orderBy($query['sort'])->paginate(50);
-        } else {
-            $sertificates = Sertificate::paginate(50);
-        }
-
-        if (array_key_exists('q', $query) and !empty($query['q'])) {
-            $q = $query['q'];
-            if(!strpos($q, ':')) {
-
-                $sertificates = Sertificate::where('full_name', 'like', '%'.$q.'%')
-                    ->orWhere('id', $q)
-                    ->orWhere('telephone', 'like', '%'.$q.'%')
-                    ->orWhereRaw("DATE_FORMAT(created_at, '%m.%d.%Y') LIKE ?", ['%' . $q . '%'])   
-                    ->get();
-            }else {
-                [$key, $value] = explode(':', $q);
-                if($key == 'id') {
-                    $sertificates = Sertificate::where($key,  $value)->get();
-                }elseif($key == 'date' or $key == 'created_at') { 
-                    $sertificates = Sertificate::orWhereRaw("DATE_FORMAT(created_at, '%m.%d.%Y') LIKE ?", ['%' . $value . '%'])   
-                        ->get();
-                }else {
-                    $sertificates = Sertificate::where($key, 'like', '%'.$value.'%')->get();
-                }
-                
-            }
-           
-        }
-        // dd($sertificate);
-        return view('sertificates.index', ['sertificates' 
-                                        => $sertificates]);
+        
+        return view('sertificates.index', 
+            [   'sertificates' =>  isset($query['q']) && !empty($query['q']) ? $this->filters($request) : $this->sort($request)     ]
+        );
     }
 
-    public function index(Request $request)
+    private function sort1(Request $request)
     {
         $query = $request->query();
 
@@ -66,40 +33,70 @@ class SertificateController extends Controller
         $sortDirection = $sortKey && $sortKey[0] === '-' ? 'desc' : 'asc';
         $sortColumn = $sortKey ? ltrim($sortKey, '-') : null;
 
-        $sertificates = Sertificate::when($sortColumn, function ($query, $sortColumn) use ($sortDirection) {
+        return Sertificate::when($sortColumn, function ($query, $sortColumn) use ($sortDirection) {
             return $query->orderBy($sortColumn, $sortDirection);
-        })->paginate(50);
+        })->paginate(80);
+    }
+
+    private function sort(Request $request)
+    {
+        $query = $request->query();
+
+        // Сортировка
+        $sortKey = $query['sort'] ?? null;
+        $sortDirection = $sortKey && $sortKey[0] === '-' ? 'desc' : 'asc';
+        $sortColumn = $sortKey ? ltrim($sortKey, '-') : null;
+
+        // Создаем уникальный ключ кэша на основе параметров сортировки
+        $cacheKey = 'sorted_certificates_' . md5(json_encode( $query ));
+
+        // Проверяем наличие кэшированных результатов
+        return Cache::remember($cacheKey, now()->addHours(1), function () use ($sortColumn, $sortDirection) {
+            return Sertificate::when($sortColumn, function ($query) use ($sortColumn, $sortDirection) {
+                return $query->orderBy($sortColumn, $sortDirection);
+            })->paginate(80);
+        });
+    }
+
+     // Фильтрация
+    private function filters(Request $request)
+    {
+        $query = $request->query();
 
         // Фильтрация
-        if (isset($query['q']) && !empty($query['q'])) {
-            $searchQuery = $query['q'];
-            // Обработка фильтрации...
-            // Добавьте соответствующую логику для фильтрации поискового запроса
+        $searchQuery = htmlspecialchars($query['q']);
 
-            $q = $query['q'];
-            if(!strpos($q, ':')) {
+        $q = $query['q'];
 
-                $sertificates = Sertificate::where('full_name', 'like', '%'.$q.'%')
-                    ->orWhere('id', $q)
-                    ->orWhere('telephone', 'like', '%'.$q.'%')
-                    ->orWhereRaw("DATE_FORMAT(created_at, '%m.%d.%Y') LIKE ?", ['%' . $q . '%'])   
+        // Проверка наличия закэшированных результатов
+        $cacheKey = 'search_results_' . md5($searchQuery);
+        if (Cache::has($cacheKey)) {
+            return Cache::get($cacheKey);
+        }
+
+        if (!strpos($searchQuery, ':')) {
+            $results = Sertificate::where('full_name', 'like', '%' . $searchQuery. '%')->orWhere('id', $searchQuery)
+                ->orWhere('telephone', 'like', '%' . $searchQuery . '%')
+                // ->orWhereRaw("DATE_FORMAT(created_at, '%m.%d.%Y') LIKE ?", ['%' . $searchQuery . '%']) // ! TODO this bag
+                ->get();
+        } else {
+            [$key, $value] = explode(':', $searchQuery);
+
+            if ($key == 'id') {
+                $results = Sertificate::where($key, $value)->get();
+            } elseif ($key == 'date' || $key == 'created_at') {
+                $results = Sertificate::orWhereRaw("DATE_FORMAT(created_at, '%m.%d.%Y') LIKE ?", ['%' . $value . '%'])
                     ->get();
-            }else {
-                [$key, $value] = explode(':', $q);
-                if($key == 'id') {
-                    $sertificates = Sertificate::where($key,  $value)->get();
-                }elseif($key == 'date' or $key == 'created_at') { 
-                    $sertificates = Sertificate::orWhereRaw("DATE_FORMAT(created_at, '%m.%d.%Y') LIKE ?", ['%' . $value . '%'])   
-                        ->get();
-                }else {
-                    $sertificates = Sertificate::where($key, 'like', '%'.$value.'%')->get();
-                }
-                
+            } else {
+                $results = Sertificate::where($key, 'like', '%' . $value . '%')->get();
             }
         }
 
-        return view('sertificates.index', ['sertificates' => $sertificates]);
+        // Кэширование результатов на 1 час (или любое другое удобное вам время)
+        Cache::put($cacheKey, $results, now()->addHours(1));
+        return $results;
     }
+
 
     /**
      * Show the form for creating a new resource.
@@ -114,7 +111,6 @@ class SertificateController extends Controller
      */
     public function store(Request $request)
     {
-        
         $result = $request->validate([
             'full_name' => ['required', 'max:255'],
             'telephone' => ['nullable', 'unique:sertificates,telephone'],
@@ -123,11 +119,11 @@ class SertificateController extends Controller
         $result['uuid'] = (string) Str::uuid();
 
         Sertificate::create($result);
+        Cache::clear();
 
-        return  response()->json([
-            'response' => 'ok',
-            'csrf-token' => csrf_token()
-        ], 200);
+        return response()->json([
+            'response' => 'ok'
+        ], 201);
     }
 
     /**
@@ -167,6 +163,7 @@ class SertificateController extends Controller
         ]);
 
         $sertificate->update($result);
+        Cache::clear();
 
         return  response()->json([
             'response' => 'ok',
@@ -193,8 +190,8 @@ class SertificateController extends Controller
         Str::isUuid($uuid) ?? abort(404);
 
         return view('sertificates.user', 
-                ['sertificat' => Sertificate::where('uuid', $uuid)->first() ?? abort(404)
-            ]);
+            [ 'sertificat' => Sertificate::where('uuid', $uuid)->first() ?? abort(404)
+        ]);
     }
 
     
--- public/assets/pages/sertificates-create.js
+++ public/assets/pages/sertificates-create.js
@@ -1,1 +1,19 @@
-$(document).ready((function(){$("#form").on("submit",(function(e){e.preventDefault(),$.ajax({url:$(this).context.action,method:"POST",dataType:"json",data:$(this).serializeArray(),success:function(e){var t=$("#message").val();$("#form")[0].reset(),$("#message").val(t)},error:function(e){}})}))}));
+$(document).ready(function () {
+    $("#form").on("submit", function (e) {
+        e.preventDefault(),
+            $.ajax({
+                url: $(this).context.action,
+                method: "POST",
+                dataType: "json",
+                data: $(this).serializeArray(),
+                success: function (e) {
+                    $("#telephone").val('');
+                    $("#full_name").val('');
+                    alert("Success add new sectificate");
+                },
+                error: function (e) {
+                    alert("Error open this console !!! "  + e.responseJSON.message);
+                },
+            });
+    });
+});
